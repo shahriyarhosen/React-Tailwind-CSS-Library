f095e0cfdfe89f6280bcc6e6fe2300e3fa3343d9
shahriyahosen <me.shahriyarhosen@gmail.com>
2022-05-21 15:19:21 +0600

    Login page init setup done

--- src/App.js
+++ src/App.js
@@ -1,4 +1,5 @@
 import "./App.css";
+import Login01 from "./Pages/Authentication/Login01";
 import Footer01 from "./Pages/Shared/Footer/Footer01";
 import Footer02 from "./Pages/Shared/Footer/Footer02";
 import Footer03 from "./Pages/Shared/Footer/Footer03";
@@ -9,18 +10,53 @@ import Footer06 from "./Pages/Shared/Footer/Footer06";
 function App() {
   return (
     <div>
-      <section className="text-center my-10">
-        <h1 className="text-2xl font-bold text-green-600">Top Footer List:</h1>
-        <p className="text-xl text-blue-500">1. Footer - 03</p>
-        <p className="text-xl text-blue-500">2. Footer - 04</p>
-        <p className="text-xl text-blue-500">2. Footer-img - 05</p>
+      <section>
+        <h1 className="text-center my-10 text-2xl font-bold text-green-600">
+          Login Page
+        </h1>
+        <div className="text-center my-10">
+          <h1 className="text-2xl font-bold text-green-600">
+            Top Login Page List:
+          </h1>
+          <p className="text-xl text-blue-500">1. Login - 01</p>
+        </div>
+        <Login01></Login01>
       </section>
-      <Footer01></Footer01>
-      <Footer02></Footer02>
-      <Footer03></Footer03>
-      <Footer04></Footer04>
-      <Footer05></Footer05>
-      <Footer06></Footer06>
+
+      <footer>
+        <div className="text-center my-10">
+          <h1 className="text-2xl font-bold text-green-600">
+            Top Footer List:
+          </h1>
+          <p className="text-xl text-blue-500">1. Footer - 03</p>
+          <p className="text-xl text-blue-500">2. Footer - 04</p>
+          <p className="text-xl text-blue-500">2. Footer-img - 05</p>
+        </div>
+        <h2 className="text-2xl text-blue-900 font-bold text-center my-5">
+          1. Footer 01
+        </h2>
+        <Footer01></Footer01>
+        <h2 className="text-2xl text-blue-900 font-bold text-center my-5">
+          2. Footer 02
+        </h2>
+        <Footer02></Footer02>
+        <h2 className="text-2xl text-blue-900 font-bold text-center my-5">
+          3. Footer 03
+        </h2>
+        <Footer03></Footer03>
+        <h2 className="text-2xl text-blue-900 font-bold text-center my-5">
+          4. Footer 04
+        </h2>
+        <Footer04></Footer04>
+        <h2 className="text-2xl text-blue-900 font-bold text-center my-5">
+          5. Footer _image_ 05
+        </h2>
+        <Footer05></Footer05>
+        <h2 className="text-2xl text-blue-900 font-bold text-center mt-10">
+          6. Footer 06
+        </h2>
+        <Footer06></Footer06>
+      </footer>
     </div>
   );
 }
