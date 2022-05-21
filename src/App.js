6a51a4e21e04dd34adbeb528e8feb728e25779fd
shahriyahosen <me.shahriyarhosen@gmail.com>
2022-05-21 10:39:56 +0600

    setup tailwind install & footer 01 add done

--- src/App.js
+++ src/App.js
@@ -1,23 +1,12 @@
-import logo from './logo.svg';
-import './App.css';
+import "./App.css";
+import Footer01 from "./Pages/Shared/Footer/Footer01";
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+      <h1>Hello for Tailwind css</h1>
+      <button className="btn btn-primary">button</button>
+      <Footer01></Footer01>
     </div>
   );
 }
