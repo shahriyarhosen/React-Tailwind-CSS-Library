5fae92bd9de6abdd1100ba765aabe5ca6a252216
shahriyahosen <me.shahriyarhosen@gmail.com>
2022-05-21 11:35:24 +0600

    Footer 3,4 & 5 done

--- src/App.js
+++ src/App.js
@@ -1,16 +1,36 @@
 import "./App.css";
 import Footer01 from "./Pages/Shared/Footer/Footer01";
 import Footer02 from "./Pages/Shared/Footer/Footer02";
+import Footer03 from "./Pages/Shared/Footer/Footer03";
+import Footer04 from "./Pages/Shared/Footer/Footer04";
+import Footer05 from "./Pages/Shared/Footer/Footer05";
 
 function App() {
   return (
     <div>
-      <h1>Hello for Tailwind css</h1>
-      <button className="btn btn-primary">button</button>
+      <section className="text-center my-10">
+        <h1 className="text-2xl font-bold text-green-600">Top Footer List:</h1>
+        <p className="text-xl text-blue-500">1. Footer - 03</p>
+        <p className="text-xl text-blue-500">2. Footer - 04</p>
+        <p className="text-xl text-blue-500">2. Footer-img - 05</p>
+      </section>
       {/* <Footer01></Footer01> */}
-      <Footer02></Footer02>
+      {/* <Footer02></Footer02> */}
+      {/* <Footer03></Footer03> */}
+      {/* <Footer04></Footer04> */}
+      <Footer05></Footer05>
     </div>
   );
 }
 
 export default App;
+
+/**
+ * Others elements:
+ * =========Link ==========
+ * 1. https://tailwind-elements.com/docs/standard/navigation/footer/
+ * 2.
+ * 3.
+ * 4.
+ * .5.
+ */
---
+++ src/Pages/Shared/Footer/Footer05.js
@@ -0,0 +1,69 @@
+import React from "react";
+
+const Footer05 = () => {
+  return (
+    <footer
+      class="text-center text-white"
+      style={{ backgroundColor: "#caced1" }}
+    >
+      <div class="container p-6 block mx-auto">
+        <div class="grid lg:grid-cols-6 md:grid-cols-3 gap-4">
+          <div class="lg:mb-0 mb-6">
+            <img
+              src="https://mdbcdn.b-cdn.net/img/new/fluid/city/113.webp"
+              class="w-full rounded-md shadow-lg"
+              alt=""
+            />
+          </div>
+          <div class="lg:mb-0 mb-6">
+            <img
+              src="https://mdbcdn.b-cdn.net/img/new/fluid/city/111.webp"
+              class="w-full rounded-md shadow-lg"
+              alt=""
+            />
+          </div>
+          <div class="lg:mb-0 mb-6">
+            <img
+              src="https://mdbcdn.b-cdn.net/img/new/fluid/city/112.webp"
+              class="w-full rounded-md shadow-lg"
+              alt=""
+            />
+          </div>
+          <div class="lg:mb-0 mb-6">
+            <img
+              src="https://mdbcdn.b-cdn.net/img/new/fluid/city/114.webp"
+              class="w-full rounded-md shadow-lg"
+              alt=""
+            />
+          </div>
+          <div class="lg:mb-0 mb-6">
+            <img
+              src="https://mdbcdn.b-cdn.net/img/new/fluid/city/115.webp"
+              class="w-full rounded-md shadow-lg"
+              alt=""
+            />
+          </div>
+          <div class="lg:mb-0 mb-6">
+            <img
+              src="https://mdbcdn.b-cdn.net/img/new/fluid/city/116.webp"
+              class="w-full rounded-md shadow-lg"
+              alt=""
+            />
+          </div>
+        </div>
+      </div>
+
+      <div
+        class="text-center p-4"
+        style={{ backgroundColor: "rgba(0, 0, 0, 0.2)" }}
+      >
+        © 2021 Copyright:
+        <a class="text-white" href="https://tailwind-elements.com/">
+          Tailwind Elements
+        </a>
+      </div>
+    </footer>
+  );
+};
+
+export default Footer05;
