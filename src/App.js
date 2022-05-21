802b61413f3743cbdaa63c08d4621e90cc6a1111
shahriyahosen <me.shahriyarhosen@gmail.com>
2022-05-21 11:10:22 +0600

    Footer 02 add done

--- src/App.js
+++ src/App.js
@@ -1,12 +1,14 @@
 import "./App.css";
 import Footer01 from "./Pages/Shared/Footer/Footer01";
+import Footer02 from "./Pages/Shared/Footer/Footer02";
 
 function App() {
   return (
     <div>
       <h1>Hello for Tailwind css</h1>
       <button className="btn btn-primary">button</button>
-      <Footer01></Footer01>
+      {/* <Footer01></Footer01> */}
+      <Footer02></Footer02>
     </div>
   );
 }
